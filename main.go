6a794c5ec84884421db51bd788641d03c1a6d0d5
Eric Lang <94770511+88potatoes@users.noreply.github.com>
2025-07-05 05:42:15 +1000

    refactor auth

--- main.go
+++ main.go
@@ -2,123 +2,15 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"time"
 
-	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/option"
-	"os/exec"
-	"runtime"
 )
-
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
-}
-
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	// The RedirectURL in the config must be updated to our local server
-	config.RedirectURL = "http://localhost:8079"
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-
-	fmt.Println("Your browser should open automatically for authentication.")
-	fmt.Printf("If it doesn't, please open this link: \n%s\n", authURL)
-
-	// Create a channel to receive the authorization code
-	codeCh := make(chan string)
-
-	// Start a local server to handle the redirect
-	srv := &http.Server{Addr: ":8079"}
-	openBrowser(authURL)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		code := r.URL.Query().Get("code")
-		if code == "" {
-			fmt.Fprintln(w, "Error: Authorization code not found. Please try again.")
-			log.Println("Warning: Could not find 'code' in GET request.")
-			return
-		}
-		fmt.Fprintln(w, "Authentication successful! You can close this browser tab now.")
-		// Send the code to the main goroutine
-		codeCh <- code
-	})
-
-	// Start the server in a new goroutine
-	go func() {
-		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-			log.Fatalf("ListenAndServe(): %v", err)
-		}
-	}()
-
-	// Wait for the authorization code
-	authCode := <-codeCh
-
-	// Shutdown the server gracefully
-	if err := srv.Shutdown(context.Background()); err != nil {
-		log.Printf("Server Shutdown Failed:%+v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
-}
-
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
-
-func openBrowser(url string) {
-	var err error
-	switch runtime.GOOS {
-	case "linux":
-		err = exec.Command("xdg-open", url).Start()
-	case "windows":
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		err = exec.Command("open", url).Start()
-	default:
-		err = fmt.Errorf("unsupported platform")
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 func main() {
 	ctx := context.Background()
@@ -126,7 +18,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
-
 
 	// If modifying these scopes, delete your previously saved token.json.
 	config, err := google.ConfigFromJSON(b, calendar.CalendarReadonlyScope)
@@ -158,4 +49,3 @@ func main() {
 		}
 	}
 }
-
